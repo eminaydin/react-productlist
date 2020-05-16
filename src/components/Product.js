421b9ea8a26794999d0cd3178ff31b9e18c3f937
eminaydin <eminaydin93@yahoo.com>
2020-05-16 03:49:04 +0200

    New Product design

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,44 +1,71 @@
 import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import "../App.scss"
+import { MdKeyboardBackspace } from "react-icons/md";
 
 const Product = ({ match, data, history }) => {
-    const [index, setIndex] = useState(0);
-    const product = data.find(p => p.id === match.params.productId);
-    let person = data[index];
+    let [index, setIndex] = useState(0);
+    const current = data.find(p => p.id === match.params.productId);
+    let product = data[index];
 
     useEffect(() => {
-        let productIndex = data.indexOf(product)
+        let productIndex = data.indexOf(current)
         setIndex(productIndex)
         return () => {
 
         };
-    }, []);
+    }, [current, data]);
 
     function backHandle() {
-        setIndex(index - 1)
-        history.push(`/products/${person.id}`)
+        setIndex(index -= 1)
+        history.replace(`/products/${product.id}`)
     }
     function nextHandle() {
-        setIndex(index + 1)
-        history.push(`/products/${person.id}`)
+        setIndex(index += 1)
+        history.replace(`/products/${product.id}`)
 
     }
-    function back() {
-        history.goBack();
-    }
+
     return (
 
-        <div>
-            <div>
-                <h3><span onClick={back}>  Return Back</span> {person.name} </h3>
-                <img src={person.image}></img>
-                <p>{person.description}</p>
-                <p>{person.price}</p>
-                <button onClick={backHandle} disabled={index <= 0 && true}> previous</button>
-                <button onClick={nextHandle} disabled={index >= 39 && true}>next</button>
+        <main>
+            <div className="card">
+                <div className="card__title">
+                    <div className="icon" onClick={() => history.goBack()} disabled={index <= 0 && true}>
+                        <a href="#"> <MdKeyboardBackspace /></a>
+                    </div>
+                    <h3>New products</h3>
+                </div>
+                <div className="card__body">
+                    <div className="half">
+                        <div className="featured_text">
+                            <h1>{product.name}</h1>
+                            <p className="sub">Office Chair</p>
+                            <p className="price">{product.price}</p>
+                        </div>
+                        <div className="image">
+                            <img src={product.image} alt="" />
+                        </div>
+                    </div>
+                    <div className="half">
+                        <div className="description">
+                            <p>{product.description}</p>
+                        </div>
+                        <span className="stock"><i className="fa fa-pen"></i> In stock</span>
+                        <div className="reviews">
+                            <span>(64 reviews)</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="card__footer">
+                    <div className="recommend">
+                        <button type="button" onClick={backHandle} disabled={index <= 0 && true}>Next Product</button>
+                    </div>
+                    <div className="action">
+                        <button type="button" onClick={nextHandle} disabled={index >= 39 && true}>Next Product</button>
+                    </div>
+                </div>
             </div>
-
-        </div >
+        </main>
     );
 }
 
