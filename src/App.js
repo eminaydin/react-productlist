e1dbfb68e0f61b76e7241ce2d7a608e047521be4
eminaydin <eminaydin93@yahoo.com>
2020-05-18 13:08:44 +0200

    Sorting fix

--- src/App.js
+++ src/App.js
@@ -10,16 +10,31 @@ import Products from './components/Products';
 import Home from './components/Home';
 import Product from "./components/Product";
 import data from "./data/products.json";
-import { AnimatePresence } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
 
 function App() {
-  data.sort((a, b) => parseFloat(b.price) - parseFloat(a.price));
-  const fourItems = data.slice(0, 4)
+  const dataArray = [...data]
+  dataArray.sort((a, b) => parseFloat(b.price) - parseFloat(a.price));
+  const fourItems = dataArray.slice(0, 4)
 
+  const pageVariants = {
+    initial: {
+      x: 0,
+    },
+    in: {
+      x: 50,
+    },
+    out: {
+      x: 0,
+    },
+  }
 
   return (
     <Router>
-      <div>
+      <motion.div initial="initial"
+        animate="in"
+        exit="out"
+        variants={pageVariants}>
         <nav>
           <ul>
             <li>
@@ -32,12 +47,12 @@ function App() {
         </nav>
         <AnimatePresence>
           <Switch>
-            <Route path={`/products/:productId`} render={(props) => <Product data={data} {...props} />} />
-            <Route path="/products" render={(props) => <Products {...props} />} />
-            <Route path="/" render={(props) => <Home data={fourItems} {...props} />} />
+            <Route path={`/products/:productId`} render={(props) => <Product data={dataArray} animation={pageVariants}{...props} />} />
+            <Route path="/products" render={(props) => <Products animation={pageVariants} {...props} />} />
+            <Route path="/" render={(props) => <Home data={fourItems} animation={pageVariants} {...props} />} />
           </Switch>
         </AnimatePresence>
-      </div>
+      </motion.div>
     </Router>
   );
 }
