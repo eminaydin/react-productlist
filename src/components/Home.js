f7b12e9560423f0f625b3f089786c36c1ee612eb
eminaydin <eminaydin93@yahoo.com>
2020-10-26 22:12:07 +0100

    More refactoring

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { motion } from "framer-motion"
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 const Home = ({ data, animation }) => {
-    return (
-        <motion.div
-            initial="initial"
-            animate="in"
-            exit="out"
-            variants={animation}>
-            <h1>Welcome user</h1>
-            <div className="boxes">
-                {data.map(({ price, name, shortDescription, slug, id }) => {
-                    const currencyFormat = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(price);
-                    return <Link to={`products/${slug}`} className="item" key={id}>
-                        <h2>{name}</h2>
-                        <p>{shortDescription}</p>
-                        <span className="price">{currencyFormat}</span>
-                    </Link>
-                })}
-            </div>
-        </motion.div >
-    );
-}
+  return (
+    <motion.div initial="initial" animate="in" exit="out" variants={animation}>
+      <h1 className="popular-products">Popular Products</h1>
+      <div className="boxes">
+        {data.map(({ price, name, shortDescription, slug, id }) => {
+          const currencyFormat = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR",
+          }).format(price);
+          return (
+            <Link to={`products/${slug}`} className="item" key={id}>
+              <h2>{name}</h2>
+              <p>{shortDescription}</p>
+              <span className="price">{currencyFormat}</span>
+            </Link>
+          );
+        })}
+      </div>
+    </motion.div>
+  );
+};
 
 export default Home;
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,80 +1,98 @@
-import React, { useState, useEffect } from 'react';
-import "../App.scss"
+import React, { useState, useEffect } from "react";
+import "../App.scss";
 import { MdKeyboardBackspace } from "react-icons/md";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
-const Product = ({ match, data, history, animation, location }) => {
-    let [index, setIndex] = useState(0);
-    let product = data[index];
-    let randomNumber = Math.floor(Math.random() * (100 - 50) + 50);
-    const currencyFormat = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(product.price);
+const Product = ({ match, data, history, animation }) => {
+  let [index, setIndex] = useState(0);
+  let product = data[index];
+  let randomNumber = Math.floor(Math.random() * (100 - 50) + 50);
+  const currencyFormat = new Intl.NumberFormat("de-DE", {
+    style: "currency",
+    currency: "EUR",
+  }).format(product.price);
 
-    useEffect(() => {
-        const currentItem = data.find(eachProduct => eachProduct.slug === match.params.slug);
-        let indexOfProduct = data.indexOf(currentItem)
-        setIndex(indexOfProduct)
-    }, []);
-
-    function clickHandler(e) {
-        const target = e.currentTarget.name;
-        if (target === "next") {
-            const nextProduct = data[index + 1]
-            setIndex(index + 1)
-            history.replace(`/products/${nextProduct.slug}`)
-        } else {
-            const prevProduct = data[index - 1]
-            setIndex(index - 1)
-            history.replace(`/products/${prevProduct.slug}`)
-        }
-    }
-
-
-
-    return (
-
-        <motion.main initial="initial"
-            animate="in"
-            exit="out"
-            variants={animation}>
-            <div className="card">
-                <div className="card__title">
-                    <div className="icon" onClick={() => history.replace("/products")} >
-                        <a href="#"> <MdKeyboardBackspace /></a>
-                    </div>
-                    <h3>New products</h3>
-                </div>
-                <div className="card__body">
-                    <div className="half">
-                        <div className="featured_text">
-                            <h1>{product.name}</h1>
-                            <p className="sub">Office Chair</p>
-                            <p className="price">{currencyFormat}</p>
-                        </div>
-                        <div className="image">
-                            <img src={product.image} alt="" />
-                        </div>
-                    </div>
-                    <div className="half">
-                        <div className="description">
-                            <p>{product.description}</p>
-                        </div>
-                        <span className="stock"><i className="fa fa-pen"></i> In stock</span>
-                        <div className="reviews">
-                            <span>({randomNumber} reviews)</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="card__footer">
-                    <div className="recommend">
-                        <button type="button" name="back" onClick={clickHandler} disabled={index <= 0 && true}>Previous Product</button>
-                    </div>
-                    <div className="action">
-                        <button type="button" name="next" onClick={clickHandler} disabled={index >= 39 && true}>Next Product</button>
-                    </div>
-                </div>
-            </div>
-        </motion.main>
+  useEffect(() => {
+    const currentItem = data.find(
+      (eachProduct) => eachProduct.slug === match.params.slug
     );
-}
+    let indexOfProduct = data.indexOf(currentItem);
+    setIndex(indexOfProduct);
+  }, [data, match.params.slug]);
+
+  function clickHandler(e) {
+    const target = e.currentTarget.name;
+    if (target === "next") {
+      const nextProduct = data[index + 1];
+      setIndex(index + 1);
+      history.replace(`/products/${nextProduct.slug}`);
+    } else {
+      const prevProduct = data[index - 1];
+      setIndex(index - 1);
+      history.replace(`/products/${prevProduct.slug}`);
+    }
+  }
+
+  return (
+    <motion.main initial="initial" animate="in" exit="out" variants={animation}>
+      <div className="card">
+        <div className="card__title">
+          <div className="icon" onClick={() => history.replace("/products")}>
+            <a href="#">
+              {" "}
+              <MdKeyboardBackspace />
+            </a>
+          </div>
+          <h3>New products</h3>
+        </div>
+        <div className="card__body">
+          <div className="half">
+            <div className="featured_text">
+              <h1>{product.name}</h1>
+              <p className="sub">Office Chair</p>
+              <p className="price">{currencyFormat}</p>
+            </div>
+            <div className="image">
+              <img src={product.image} alt="" />
+            </div>
+          </div>
+          <div className="half">
+            <div className="description">
+              <p>{product.description}</p>
+            </div>
+            <span className="stock">
+              <i className="fa fa-pen"></i> In stock
+            </span>
+            <div className="reviews">
+              <span>({randomNumber} reviews)</span>
+            </div>
+          </div>
+        </div>
+        <div className="card__footer">
+          <div className="recommend">
+            <button
+              type="button"
+              name="back"
+              onClick={clickHandler}
+              disabled={index <= 0}
+            >
+              Previous Product
+            </button>
+          </div>
+          <div className="action">
+            <button
+              type="button"
+              name="next"
+              onClick={clickHandler}
+              disabled={index >= 39}
+            >
+              Next Product
+            </button>
+          </div>
+        </div>
+      </div>
+    </motion.main>
+  );
+};
 
 export default Product;
